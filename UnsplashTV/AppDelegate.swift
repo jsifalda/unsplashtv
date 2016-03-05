7f997833e125612d59e991ea6d5d16d5aa461543
jsifalda <sifalda.jiri@gmail.com>
2016-03-05 09:49:30 +0100

    Add source

---
+++ UnsplashTV/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  UnsplashTV
+//
+//  Created by Jiri Safalda on 05/03/16.
+//  Copyright © 2016 HackingMyLife. All rights reserved.
+//
+
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+
+    var window: UIWindow?
+    
+    var appController: TVApplicationController?
+    static let TVBaseURL = "http://localhost:9001/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)src/application.js"
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        // 1
+        let appControllerContext = TVApplicationControllerContext()
+        
+        // 2
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else {
+            fatalError("unable to create NSURL")
+        }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        // 3
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+
+}
+
